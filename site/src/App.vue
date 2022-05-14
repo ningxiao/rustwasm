6296bf8de0e4d1947a767c0bcbbd59d0a5543258
ningxiao <363305175@qq.com>
2022-05-14 23:12:15 +0800

    配置生产环境打包

--- site/src/App.vue
+++ site/src/App.vue
@@ -7,25 +7,25 @@ import init, { draw_julia, async_run_fetch, greet, add } from "../pkg/rustWasmTe
 export default defineComponent(
     {
         setup() {
-            const canvasElement = ref<HTMLCanvasElement>(null);
+            const canvasElement = ref<HTMLCanvasElement | undefined>();
             return {
                 canvasElement
             }
         },
-        mounted() {
-            const canvas = this.canvasElement
+        async mounted() {
             const ratio = window.devicePixelRatio;
             const darwText = (canvas: HTMLCanvasElement) => {
-                const ctx = canvas.getContext("2d");
-                canvas.width = canvas.offsetWidth * ratio;
-                canvas.height = canvas.offsetHeight * ratio;
-                ctx.scale(ratio, ratio);
-                ctx.font = "18px serif";
-                ctx.fillText(`Rust操作cavas对象，调用add计算 ${add(21, 34)}`, 10, 50);
-                setTimeout(() => {
-                    draw_julia(ctx, canvas.width, canvas.height, -0.15, 0.65);
-                }, 1000);
-
+                const ctx: CanvasRenderingContext2D | null = canvas.getContext("2d");
+                if (ctx) {
+                    canvas.width = canvas.offsetWidth * ratio;
+                    canvas.height = canvas.offsetHeight * ratio;
+                    ctx.scale(ratio, ratio);
+                    ctx.font = "18px serif";
+                    ctx.fillText(`Rust操作cavas对象，调用add计算 ${add(21, 34)}`, 10, 50);
+                    setTimeout(() => {
+                        draw_julia(ctx, canvas.width, canvas.height, -0.15, 0.65);
+                    }, 1000);
+                }
             }
 
             const run_rust_async_fetch = () => {
@@ -35,17 +35,17 @@ export default defineComponent(
                     console.log("%s, authored by %s <%s>", data.commit.sha, data.commit.commit.author.name, data.commit.commit.author.email);
                 });
             }
-            (async () => {
-                await init();
-                const result = add(12, 25);
-                console.log(`12 + 25 = ${result}`);
-                if (result !== 37) {
-                    throw new Error("wasm addition doesn't work!");
-                }
-                greet('World RustWasm');
-                darwText(canvas);
-                run_rust_async_fetch();
-            })();
+            await init();
+            const result = add(12, 25);
+            console.log(`12 + 25 = ${result}`);
+            if (result !== 37) {
+                throw new Error("wasm addition doesn't work!");
+            }
+            greet('World RustWasm');
+            if (this.canvasElement) {
+                darwText(this.canvasElement);
+            }
+            run_rust_async_fetch();
         }
     }
 )
--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,12 +1,39 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import importToCDN from 'vite-plugin-cdn-import';
 // https://vitejs.dev/config/
 export default defineConfig(
     {
-        plugins: [vue()],
-        server:{
-            open:true,
-            port:8080
+        plugins: [vue(),
+        importToCDN({
+            modules: [
+                {
+                    var: 'Vue',
+                    name: 'vue',
+                    path: `https://unpkg.com/vue@3.2.19/dist/vue.runtime.global.prod.js`,
+                },
+            ]
+        })],
+        server: {
+            open: true,
+            port: 8080
+        },
+        build: {
+            target: "esnext",
+            brotliSize: false, /* 压缩大型输出文件可能会很慢，因此禁用该功能可能会提高大型项目的构建性能 */
+            outDir: 'dist', /* 指定输出路径 */
+            cssCodeSplit: false, /* 整个项目中的所有 CSS 将被提取到一个 CSS 文件中 */
+            chunkSizeWarningLimit: 1500, /* chunk 大小警告的限制（以 kbs 为单位） */
+            sourcemap: false, /* 构建后是否生成 source map 文件 */
+            assetsDir: 'static/images/', /* 指定生成静态资源的存放路径 */
+            emptyOutDir: true, /* 默认情况下，若 outDir 在 root 目录下，则 Vite 会在构建时清空该目录 */
+            rollupOptions: {
+                output: {
+                    chunkFileNames: 'js/[name].[hash].js',
+                    entryFileNames: 'js/[name].[hash].js',
+                    assetFileNames: '[ext]/[name].[hash].[ext]',
+                },
+            }
         }
     }
 )
